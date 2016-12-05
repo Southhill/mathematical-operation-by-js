7d86c34c5dec4026717c1878c752f75cfb8e65a0
xfn-ll <cw3180@163.com>
2016-12-05 14:10:20 +0800

    update: 全排列的实现算法

--- src/mathOperation.js
+++ src/mathOperation.js
@@ -1,6 +1,6 @@
 import { uniqueArr } from './baseOperation.js'
 
-// 数组的交集
+// 交集：两个数组的交集处理
 function diff(arr1, arr2) {
   //对arr1,arr2先进行去重处理。
   arr1 = uniqueArr(arr1)
@@ -18,4 +18,32 @@ function diff(arr1, arr2) {
     newArr = arr1.concat(arr2);
   return newArr;
 }
-//数组的并集
+//并集：两个数组的并集处理
+
+//全排列：给定一个基本的数据类型，例：字符串，得到它的全排列，将结果存放到数组中返回。
+function permu(str) {
+  var strArr = str.split('');
+  var resultArr = [];
+  var tempArr = [];
+  var len = str.length;
+  (function rec(arr){
+    if (arr.length === 1) {
+      resultArr.push(tempArr.concat(arr).join(''));
+      tempArr.pop();
+    } else {
+      var arr2 = arr.slice(0);
+      var len2 = arr2.length;
+      for (var i = 0;i <= len2; i++) {
+        if (i < len2) {
+          tempArr.push(arr2[i]);
+          var arr3 = arr2.slice(0);
+          arr3.splice(i, 1);
+          rec(arr3);
+        } else {
+          tempArr.pop();
+        }
+      }
+    }
+  })(strArr);
+  return resultArr;
+}
