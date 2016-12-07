cf11be61ed143994702cc88ff3a54c6164cf917b
ccaoee <cw3180@163.com>
2016-12-07 09:23:37 +0800

    change test conf

--- test/Array.test.js
+++ test/Array.test.js
@@ -1,12 +1,25 @@
-import chai from 'chai'
+// import chai from 'chai'
+//
+// describe('Array',
+// 	describe('array first', () => {
+// 		it('first', () => {
+// 			expect(add(2, 3)).to.be.equal(5)
+// 		})
+// 	})
+// )
+// function add(a,b) {
+// 	return a + b
+// }
 
-describe('Array',
-	describe('array first', () => {
-		it('first', () => {
-			expect(add(2, 3)).to.be.equal(5)
-		})
-	})
-)
-function add(a,b) {
-	return a + b
+function sleep(ms = 0) {
+  return new Promise((resolve, reject) => setTimeout(resolve, ms));
 }
+
+async function test() {
+  for (let i = 0; i < 10; i++) {
+    await sleep(500);
+    console.log(`i=${i}`);
+  }
+}
+
+test().then(() => console.log('done'));
