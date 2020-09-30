54b2090b8afbd1ee23077a0341b86ddf6bfe1506
yuliren <yuliren@2dfire.com>
2020-09-30 11:37:16 +0800

    update: add gaussian_elimination method

--- src/mathOperation.js
+++ src/mathOperation.js
@@ -108,3 +108,125 @@ export function createNumArray(length, exp = 'a+1') {
 
     return Array.from(Array(length), (_, idx) => Number(a * idx) + Number(b))
 }
+
+/**
+ * 高斯消元法
+ * @param {Array[[]]} mat 方程组的增广矩阵
+ */
+function elimination(mat) { 
+  for (let i = 1,len = mat.length; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      // TODO: 被除数不能为0, 每次运算时，必须保证对角线上的元素不为0(即运算中的分母不为0)
+      const coe = mat[i][j] / mat[j][j] // 系数
+  
+      mat[i].forEach((val, idx) => {
+        mat[i][idx] -= mat[j][idx] * coe
+      })
+    }
+  }
+}
+/**
+ * 列主元消去法
+ * @param {Matrix} mat 方程组矩阵
+ */
+function columnElimination(mat) {
+  const swap = (a, b) => {
+    const temp = mat[a]
+
+    mat[a] = mat[b]
+    mat[b] = temp
+  }
+  const getMaxColumnIndex = (ci) => {
+    let result = 0
+    for (let i = 1, len = mat.length; i < len; i++) {
+      if (Math.abs(mat[i][ci]) > Math.abs(mat[result][ci])) result = i
+    }
+
+    return result
+  }
+
+  for (let i = 1,len = mat.length; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      const maxCI = getMaxColumnIndex(j)
+
+	  // 矩阵交换两行（列）不需要变号，这和行列式不同。因为：行列式是值，而矩阵是表，每一行可以看做一个记录
+      if (maxCI !== j) swap(maxCI, j)
+
+      const coe = mat[i][j] / mat[j][j] // 系数
+  
+      mat[i].forEach((val, idx) => {
+        mat[i][idx] -= mat[j][idx] * coe
+      })
+    }
+  }
+}
+
+/**
+ * 返回当前方程组的解的状态  
+ * 解的状态：无解，多解，唯一解
+ * @param {Matrix} mat 处理后的行阶梯阵
+ */
+function getSolutionStatus(mat) {
+  let status = 'unique' // 解的状态：无解，多解，唯一解
+
+  if (mat.some(row => row.slice(0, -1).every(val => val === 0) && row[row.length - 1] !== 0)) status = 'no'
+
+  if (mat.some(row => row.every(val => val === 0))) status = 'multiple'
+
+  return status
+}
+/**
+ * 回代
+ * @param {Matrix} mat 处理后的矩阵
+ */
+function back_substitution(mat) {
+  const rowLen = mat.length
+  const columnLen = mat[0].length
+  const result = Array(rowLen).fill()
+
+  for (let t = rowLen - 1; t >= 0; t--) {
+    const divisor = result.reduceRight((pre, cur, idx) => {
+      if (cur === undefined) {
+        return pre
+      } else {
+        return pre - mat[t][idx] * cur
+      }
+    }, mat[t][columnLen - 1])
+
+    result[t] = divisor / mat[t][t]
+  }
+
+  return result
+}
+
+/**
+ * 高斯消元法
+ * @param {Array[[]]} mat 方程组的增广矩阵，默认方程组的系数矩阵都是合法数值
+ */
+export function gaussian_elimination(mat) {
+  // mat 为增广矩阵
+  // 校验参数
+  const rowLen = mat.length
+  const columnLen = mat[0].length
+
+  if (columnLen - rowLen !== 1) {
+    throw new Error('mat required is the augmented matrix')
+  }
+
+  // 消元
+  elimination(mat)
+
+  // 解的判断
+  const solutionStatus = getSolutionStatus(mat) // 解的状态：无解，多解，唯一解
+
+
+  if (solutionStatus === 'no') {
+    return -1
+  } else if (solutionStatus === 'multiple') {
+    return []
+  } else {
+    // 回代
+    return back_substitution(mat)
+  }
+}
+
